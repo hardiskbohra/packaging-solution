a2376b7567b7bf8ec04d0ce2ce5f100ade278c89
Hardik Bohra <hardiskbohra@gmail.com>
2023-11-14 16:03:28 +0530

    Update sidebar.blade.php

--- resources/views/panels/sidebar.blade.php
+++ resources/views/panels/sidebar.blade.php
@@ -98,8 +98,8 @@
             <li class=" nav-link"><a href="{{route('users.index')}}"><i class="menu-livicon" data-icon="users"></i><span class="menu-title" data-i18n="User">User</span></a>
 
             </li>
-            <li class=" nav-item"><a href="#"><i class="menu-livicon" data-icon="notebook"></i><span class="menu-title" data-i18n="Invoice">Invoice</span></a>
-                <ul class="menu-content">
+            <li class=" nav-item"><a href="{{route('invoices.index')}}"><i class="menu-livicon" data-icon="notebook"></i><span class="menu-title" data-i18n="Invoice">Invoices</span></a>
+<!--                 <ul class="menu-content">
                     <li><a href="{{route('invoices.index')}}"><i class="bx bx-right-arrow-alt"></i><span class="menu-item" data-i18n="Invoice List">Invoice List</span></a>
                     </li>
                     <li><a href="app-invoice.html"><i class="bx bx-right-arrow-alt"></i><span class="menu-item" data-i18n="Invoice">Invoice</span></a>
@@ -108,7 +108,7 @@
                     </li>
                     <li class="active"><a href="{{route('invoices.create')}}"><i class="bx bx-right-arrow-alt"></i><span class="menu-item" data-i18n="Invoice Add">Invoice Add       </span></a>
                     </li>
-                </ul>
+                </ul> -->
             </li>
 
         </ul>
