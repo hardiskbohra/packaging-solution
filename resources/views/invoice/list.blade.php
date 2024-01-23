a8cf435cb2c2988f29d0fc8fcbbd5862d67fc5df
Hardik Bohra <hardiskbohra@gmail.com>
2024-01-23 19:53:59 +0530

    Update list.blade.php

--- resources/views/invoice/list.blade.php
+++ resources/views/invoice/list.blade.php
@@ -242,16 +242,16 @@
                                                                                     </div> --}}
 
 
-<!--                                                                                     <label>Payment Mode: </label>
+                                                                                    <label>Payment Mode: </label>
                                                                                     <div class="form-group">
                                                                                         <select class="form-control" id="Invoice-payment-mode" name="payment_mode">
                                                                                             <option value="cash">Cash</option>
                                                                                             <option value="card">Card</option>
                                                                                             <option value="upi">UPI</option>
                                                                                         </select>
-                                                                                    </div> -->
+                                                                                    </div>
 
-                                                                                    <div class="row">
+<!--                                                                                     <div class="row">
                                                                                         <div class="col-md-9">
                                                                                             <label>Payment Mode: </label>
                                                                                             <div class="form-group">
@@ -269,6 +269,17 @@
                                                                                                 <input type="checkbox" name="full_payment" id="checkboxGlow1" style="padding-left: 10%; border:0px;">
                                                                                                 <label for="checkboxGlow1"></label>
                                                                                             </div>
+                                                                                        </div>
+                                                                                    </div> -->
+
+                                                                                    <div class="row">
+                                                                                        <div class="widget-todo-title-area d-flex align-items-center">
+                                                                                            <i class='bx bx-grid-vertical mr-25 font-medium-4 cursor-move'></i>
+                                                                                            <div class="checkbox checkbox-shadow">
+                                                                                                <input type="checkbox" name="full_payment" id="checkboxGlow1" class="checkbox__input">
+                                                                                                <label for="checkboxGlow1"></label>
+                                                                                            </div>
+                                                                                            <span class="widget-todo-title ml-50">Adjust remaining amount as discount.</span>
                                                                                         </div>
                                                                                     </div>
 
